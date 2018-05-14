87f940352b2664110142c4fcb269fe0f326808fb
yuhy <yhy_working@163.com>
2018-05-14 18:08:45 +0800

    jmeterMaven项目

---
+++ src/main/java/com/yhy/jmeterMaven/App.java
@@ -0,0 +1,13 @@
+package com.yhy.jmeterMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
